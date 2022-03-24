f4769d4bd5b50d435699175824daa55965ee5b33
AIV420 <aiv420dev@gmail.com>
2022-03-24 23:08:48 +0000

    commint🍃

--- js/term.js
+++ js/term.js
@@ -1,6 +1,6 @@
 log("Welcome to The Terminal. Type 'help' for help.");
 
 register_cmd("calc", function(cmd) {
-    openWin("Calculator","calc");
+    openWin("Calculator","calc","250px","400px");
     log("Opened the calculator.");
 });
