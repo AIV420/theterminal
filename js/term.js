b0a114d5dd85fa7df64578e571d3fa5d3c837541
AIV420 <aiv420dev@gmail.com>
2022-03-25 16:08:47 +0000

    fixup?

--- js/term.js
+++ js/term.js
@@ -1,6 +1,6 @@
 log("Welcome to The Terminal. Type 'help' for help.");
 
 register_cmd("calc", function(cmd) {
-    openWin("Calculator","calc","250px","400px");
+    openWin("Calculator","calc","300px","450px");
     log("Opened the calculator.");
 });
