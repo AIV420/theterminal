000abcd55f333627b34880595223ee269ae7a3c3
AIV420 <aiv420dev@gmail.com>
2022-04-13 20:29:05 +0000

    added bad login and signup support

--- js/term.js
+++ js/term.js
@@ -1,3 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIGJx51zZmc_w1Y_0_BlMuyGS35_tbqq0",
+    authDomain: "theterminal-7a6a0.firebaseapp.com",
+    projectId: "theterminal-7a6a0",
+    storageBucket: "theterminal-7a6a0.appspot.com",
+    messagingSenderId: "798406253137",
+    appId: "1:798406253137:web:13807f4ccc0149ea627670",
+    measurementId: "G-HESG4X1YLZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
 log(`© <span style="color: #8be9fd;">2022</span> by Nexis Inc. Type '<b class="logcmd" onclick="cmdclick('cpright');">cpright</b>' for more information.`);
 log(`Welcome to The Terminal. Type '<b class="logcmd" onclick="cmdclick('help');">help</b>' for help.`);
 
@@ -41,6 +62,7 @@ register_cmd("games", function(cmd) {
         block_log(` &nbsp&nbsp&nbsp 3. <b class="logcmd" onclick="gameclick('advancewars', 'Advance Wars');">Advance Wars</b>`);
         block_log(` &nbsp&nbsp&nbsp 4. <b class="logcmd" onclick="gameclick('advancewars2', 'Advance Wars 2');">Advance Wars 2</b>`);
         block_log(` &nbsp&nbsp&nbsp 5. <b class="logcmd" onclick="gameclick('superstar', 'Mario & Luigi: Superstar Saga');">Mario &amp; Luigi: Superstar Saga</b>`);
+        return;
     }
     if (parameters[0].toString().toUpperCase() === "MORE") {
         if (parameters.length === 1) {
@@ -61,6 +83,7 @@ register_cmd("editor", function(cmd) {
     if (parameters.length === 0) {
         log("You did not provide a language, opened the text editor.")
         openWin("TEXT Editor","editor.html#text","700px","450px");
+        return;
     }
     if (parameters.length === 1) {
         log("Opened the editor.");
@@ -68,3 +91,59 @@ register_cmd("editor", function(cmd) {
         return;
     }
 });
+
+register_cmd("login", function(cmd) {
+    var parameters = smart_split(cmd, " ", false).slice(1);
+    if (parameters.length === 0) {
+        log("Opened the Login window.");
+        return;
+    }
+    if (parameters.length === 1) {
+        log("Please provide a password after your email.");
+        return;
+    }
+    if (parameters.length === 2) {
+        log("Logging you in...");
+        Login(parameters[0],parameters[1]);
+        return;
+    }
+});
+
+register_cmd("signup", function(cmd) {
+    var parameters = smart_split(cmd, " ", false).slice(1);
+    if (parameters.length === 0) {
+        log("Opened the Sign Up window.");
+        return;
+    }
+    if (parameters.length === 1) {
+        log("Please provide a password after your email.");
+        return;
+    }
+    if (parameters.length === 2) {
+        log("Signing you up...");
+        Signup(parameters[0],parameters[1]);
+        return;
+    }
+});
+
+function Login(email,password) {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        log("Logged in with email "+email+".");
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        log(error.code);
+    });
+}
+
+function Signup(email,password) {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        log("Signed up and logged in with email "+email+".");
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        log(error.code);
+    });
+}
